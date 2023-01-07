cede764cb9d07a6c6488f68db2d2c2013a2c50c7
mingwandowski <haoming2019@gmai.com>
2023-01-07 15:19:44 +0100

    init

---
+++ src/main/java/com/mingwandowski/soommateboot/SoommateBootApplication.java
@@ -0,0 +1,13 @@
+package com.mingwandowski.soommateboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoommateBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoommateBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mingwandowski/soommateboot/SoommateBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingwandowski.soommateboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoommateBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
