517dc63307f56bb8b5fd08f09654e80b82e27402
mingwandowski <haoming2019@gmai.com>
2023-01-09 15:22:16 +0100

    update login-component ui

--- soommate-ui/src/App.js
+++ soommate-ui/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './components/Header';
-import HomeSignUp from './components/HomeSignUp';
+import Introduction from './components/start_component/Introduction';
+import HomeSignUp from './components/start_component/HomeSignUp';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HomeSignUp />
+      <div className='login-component row'>
+        <div className='introduction col-lg-6'>
+          <Introduction />
+        </div>
+        <div className='signin-signup col-lg-6'>
+          <HomeSignUp />
+        </div>
+      </div>
+      <div className='main-component'>
+      </div>
     </div>
   );
 }
---
+++ soommate-ui/src/components/start_component/Introduction.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Introduction() {
+    return (
+        <div>
+            <h1>How to Use</h1>
+        </div>
+    );
+}
+
+export default Introduction;
