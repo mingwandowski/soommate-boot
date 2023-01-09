8d358bbf6457310517c9273cc3ae30d8fa70987d
mingwandowski <haoming2019@gmai.com>
2023-01-09 17:00:09 +0100

    add homeSignIn template

--- soommate-ui/src/App.js
+++ soommate-ui/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Introduction from './components/start_component/Introduction';
 import HomeSignUp from './components/start_component/HomeSignUp';
+import HomeSignIn from './components/start_component/HomeSignIn';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
           <Introduction />
         </div>
         <div className='signin-signup col-lg-6'>
-          <HomeSignUp />
+          {/* <HomeSignUp /> */}
+          <HomeSignIn />
         </div>
       </div>
       <div className='main-component'>
