866779ea0f111f943d70e07faf61b0556c4ecb84
mingwandowski <haoming2019@gmai.com>
2023-01-13 11:51:22 +0100

    fix className warn

--- soommate-ui/src/components/main_component/ResultPage.jsx
+++ soommate-ui/src/components/main_component/ResultPage.jsx
@@ -9,48 +9,48 @@ function ResultPage(props) {
         <div className='result-page card card-registration my-4'>
             <h2>You Saved {result.averageSaved} Each!</h2>
             <div className="card result-card">
-                <div class="card-header">
-                    <h5 class="card-title">{props.homeInfo.room1Name}</h5>
+                <div className="card-header">
+                    <h5 className="card-title">{props.homeInfo.room1Name}</h5>
                 </div>
                 <div className="card-body row">
-                    <h6 class="card-subtitle col-lg-7">{result.room1User}</h6>
-                    <h6 class="card-text col-lg-5">${result.room1Price}</h6>
+                    <h6 className="card-subtitle col-lg-7">{result.room1User}</h6>
+                    <h6 className="card-text col-lg-5">${result.room1Price}</h6>
                 </div>
             </div>
             <div className="card result-card">
-                <div class="card-header">
-                    <h5 class="card-title">{props.homeInfo.room2Name}</h5>
+                <div className="card-header">
+                    <h5 className="card-title">{props.homeInfo.room2Name}</h5>
                 </div>
                 <div className="card-body row">
-                    <h6 class="card-subtitle col-lg-7">{result.room2User}</h6>
-                    <h6 class="card-text col-lg-5">${result.room2Price}</h6>
+                    <h6 className="card-subtitle col-lg-7">{result.room2User}</h6>
+                    <h6 className="card-text col-lg-5">${result.room2Price}</h6>
                 </div>
             </div>
             <div className="card result-card" style={{display: props.homeInfo.numOfRooms > 2 ? "" : "none"}}>
-                <div class="card-header">
-                    <h5 class="card-title">{props.homeInfo.room3Name}</h5>
+                <div className="card-header">
+                    <h5 className="card-title">{props.homeInfo.room3Name}</h5>
                 </div>
                 <div className="card-body row">
-                    <h6 class="card-subtitle col-lg-7">{result.room3User}</h6>
-                    <h6 class="card-text col-lg-5">${result.room3Price}</h6>
+                    <h6 className="card-subtitle col-lg-7">{result.room3User}</h6>
+                    <h6 className="card-text col-lg-5">${result.room3Price}</h6>
                 </div>
             </div>
             <div className="card result-card" style={{display: props.homeInfo.numOfRooms > 3 ? "" : "none"}}>
-                <div class="card-header">
-                    <h5 class="card-title">{props.homeInfo.room4Name}</h5>
+                <div className="card-header">
+                    <h5 className="card-title">{props.homeInfo.room4Name}</h5>
                 </div>
                 <div className="card-body row">
-                    <h6 class="card-subtitle col-lg-7">{result.room4User}</h6>
-                    <h6 class="card-text col-lg-5">${result.room4Price}</h6>
+                    <h6 className="card-subtitle col-lg-7">{result.room4User}</h6>
+                    <h6 className="card-text col-lg-5">${result.room4Price}</h6>
                 </div>
             </div>
             <div className="card result-card" style={{display: props.homeInfo.numOfRooms > 4 ? "" : "none"}}>
-                <div class="card-header">
-                    <h5 class="card-title">{props.homeInfo.room5Name}</h5>
+                <div className="card-header">
+                    <h5 className="card-title">{props.homeInfo.room5Name}</h5>
                 </div>
                 <div className="card-body row">
-                    <h6 class="card-subtitle col-lg-7">{result.room5User}</h6>
-                    <h6 class="card-text col-lg-5">${result.room5Price}</h6>
+                    <h6 className="card-subtitle col-lg-7">{result.room5User}</h6>
+                    <h6 className="card-text col-lg-5">${result.room5Price}</h6>
                 </div>
             </div>
 
