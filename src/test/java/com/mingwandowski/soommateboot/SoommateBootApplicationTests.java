70c7dec9cf2f9a00fcc6e257e8701470a9783e32
mingwandowski <haoming2019@gmai.com>
2023-01-16 21:15:08 +0100

    solve mvn test problem

--- src/test/java/com/mingwandowski/soommateboot/SoommateBootApplicationTests.java
+++ src/test/java/com/mingwandowski/soommateboot/SoommateBootApplicationTests.java
@@ -1,9 +1,10 @@
 package com.mingwandowski.soommateboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringBootApplication.class)
 class SoommateBootApplicationTests {
 
 	@Test
